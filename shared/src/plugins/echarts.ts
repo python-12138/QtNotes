59c78d32e93bd93741ea5e53839bf8a07fe52155
r e <err347044@outlook.com>
2026-09-20 09:59:05 +0800

    修复：行驶里程折线图空白（ECharts 未注册 LineChart）

--- shared/src/plugins/echarts.ts
+++ shared/src/plugins/echarts.ts
@@ -2,7 +2,7 @@
 // 在 main.ts 里 import 本文件一次即可全局生效。
 import { use } from 'echarts/core';
 import { CanvasRenderer } from 'echarts/renderers';
-import { PieChart, BarChart } from 'echarts/charts';
+import { PieChart, BarChart, LineChart } from 'echarts/charts';
 import { GridComponent, TooltipComponent, LegendComponent } from 'echarts/components';
 
-use([CanvasRenderer, PieChart, BarChart, GridComponent, TooltipComponent, LegendComponent]);
+use([CanvasRenderer, PieChart, BarChart, LineChart, GridComponent, TooltipComponent, LegendComponent]);
